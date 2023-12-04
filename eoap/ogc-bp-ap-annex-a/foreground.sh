84bb57dea3fb360d943147e19a9495d39075d986
Fabrice Brito <fabrice.brito@terradue.com>
2023-12-04 14:12:23 +0100

    fix

--- eoap/ogc-bp-ap-annex-a/foreground.sh
+++ eoap/ogc-bp-ap-annex-a/foreground.sh
@@ -6,3 +6,5 @@ while [ ! -f /tmp/finished ]; do sleep 1; done
 echo "Done"
 
 pip install stac-asset pystac
+
+export PS1="\[\](base) \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ \[\]"
