1098c191eb8e23cb470b827fa49ec2eb034482e4
Fabrice Brito <fabrice.brito@terradue.com>
2023-12-04 17:56:56 +0100

    calrissian WIP

--- eoap/calrissian-hands-on/foreground.sh
+++ eoap/calrissian-hands-on/foreground.sh
@@ -2,4 +2,4 @@
 
 export PS1="\[\](base) \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ \[\]"
 
-curl 
+curl https://raw.githubusercontent.com/eoap/kc-eoap/main/eoap/calrissian-hands-on/install.sh | sh
