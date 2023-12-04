a6f2f0b0c3254ced1ff0e725a134fe8e05585b50
Fabrice Brito <fabrice.brito@terradue.com>
2023-12-04 10:21:14 +0100

    creates d1 params

--- eoap/ogc-bp-ap-annex-a/background.sh
+++ eoap/ogc-bp-ap-annex-a/background.sh
@@ -4,10 +4,10 @@ pip install cwltool
 
 wget https://raw.githubusercontent.com/eoap/ogc-bp-ap-annex-a-examples/main/D.1.%20Crop%20Application%20Example/app-package.cwl -O d1-app-package.cwl
 
-wget https://raw.githubusercontent.com/eoap/ogc-bp-ap-annex-a-examples/main/D.1.%20Crop%20Application%20Example/params.yml -O d1-params.yml
-
 wget https://raw.githubusercontent.com/eoap/ogc-bp-ap-annex-a-examples/main/stage.py -O stage.py
 
 pip install stac-asset pystac
+
+apt-get install -y tree
 
 touch /tmp/finished
