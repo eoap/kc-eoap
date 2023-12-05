2e2a887302dc66f763f12888a4abf3021170146d
Fabrice Brito <fabrice.brito@terradue.com>
2023-12-05 15:50:48 +0100

    debug

--- eoap/calrissian-hands-on/foreground.sh
+++ eoap/calrissian-hands-on/foreground.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PS1="\[\](base) \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ \[\]"
+export PS1="\[\](base) \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ \[\]" 2>&1
 
-curl -q https://raw.githubusercontent.com/eoap/kc-eoap/main/eoap/calrissian-hands-on/install.sh | sh
+curl --silent https://raw.githubusercontent.com/eoap/kc-eoap/main/eoap/calrissian-hands-on/install.sh | sh
--- eoap/calrissian-hands-on/install.sh
+++ eoap/calrissian-hands-on/install.sh
@@ -4,7 +4,7 @@ apt update > /dev/null 2>&1
 apt install -y nfs-common  > /dev/null 2>&1
 
 echo "2. Install nfs-common on node01"
-ssh 172.30.2.2 "apt update; apt-get install -y nfs-common" > /dev/null 2>&1
+ssh 172.30.2.2 "apt update; apt-get install -y nfs-common" # > /dev/null 2>&1
 
 echo "3. Install OpenEBS"
 helm repo add openebs-nfs https://openebs.github.io/dynamic-nfs-provisioner  > /dev/null 2>&1
@@ -37,10 +37,5 @@ spec:
 EOF
 
 
-stac_items:
-- "https://earth-search.aws.element84.com/v0/collections/sentinel-s2-l2a-cogs/items/S2A_10TFK_20210708_0_L2A"
-- "https://earth-search.aws.element84.com/v0/collections/sentinel-s2-l2a-cogs/items/S2B_10TFK_20210713_0_L2A"
-aoi: "-121.399,39.834,-120.74,40.472"
-epsg: "EPSG:4326"
 
 
